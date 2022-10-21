74bb996e63ec488fca3446ec8d2835c41c61d9aa
David Cooper <davidpcooper1@gmail.com>
2022-10-21 00:01:35 -0500

    Intial structure

---
+++ packages/core/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    transform: {'^.+\\.ts?$': 'ts-jest' },
+    testEnvironment: 'node',
+    //testMatch: ['**/*\\.(test|spec)\\.(ts|tsx)$'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
